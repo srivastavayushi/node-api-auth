6f67e38e3a0dfbd30186100f6af41f8d4df690f4
srivastavayushi <srivastavayushi@users.noreply.github.com>
2021-05-31 12:46:34 +0530

    feat : validate user details

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+  // Validate the data before registering user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const { name, email, password } = req.body;
+  // create a new user
   const user = new User({
     name,
     email,
---
+++ validation.js
@@ -0,0 +1,14 @@
+// Validation Register
+const Joi = require("@hapi/joi");
+
+const registerValidation = (data) => {
+  const schema = {
+    name: Joi.string().required().min(6),
+    email: Joi.string().required().email().min(6),
+    password: Joi.string().required().min(6),
+  };
+
+  return Joi.validate(data, schema);
+};
+
+module.exports.registerValidation = registerValidation;
